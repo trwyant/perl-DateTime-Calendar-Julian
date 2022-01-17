a03b69c8201de1099733fa0bf220154cbf663528
Tom Wyant <wyant@cpan.org>
2022-01-17 12:00:02 -0500

    Update copyright to 2022.

--- lib/DateTime/Calendar/Julian.pm
+++ lib/DateTime/Calendar/Julian.pm
@@ -206,7 +206,7 @@ Thomas R. Wyant, III F<wyant at cpan dot org>
 
 Copyright (c) 2003 Eugene van der Pijll.  All rights reserved.
 
-Copyright (C) 2018-2021 Thomas R. Wyant, III
+Copyright (C) 2018-2022 Thomas R. Wyant, III
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
